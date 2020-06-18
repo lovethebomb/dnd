3e128c57d2433f8f72ccb9abedfe9d241a633061
Lucas Heymès <lucas@lovethebomb.eu>
2020-06-18 16:19:24 +0200

    Disable Electrum by default

--- components/TreasureSplitter/context/CurrencyContext.js
+++ components/TreasureSplitter/context/CurrencyContext.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const DEFAULT_CURRENCY = {
   platinum: { name: 'Platinum', short: 'pp', value: 0, toCP: 1000, enabled: true },
   gold: { name: 'Gold', short: 'gp', value: 8, toCP: 100, enabled: true },
-  electrum: { name: 'Electrum',  short: 'ep', value: 0, toCP: 50, enabled: true },
+  electrum: { name: 'Electrum',  short: 'ep', value: 0, toCP: 50, enabled: false },
   silver: { name: 'Silver', short: 'sp', value: 4, toCP: 10, enabled: true },
   copper: { name: 'Copper', short: 'cp', value: 0, toCP: 1, enabled: true }
 }

c3da5b0538c7636dfbd2e346ada3e346e63a0a8a
Lucas Heymès <lucas@lovethebomb.eu>
2020-06-18 16:51:02 +0200

    Add default background-color to PartyMemberIcon

--- components/TreasureSplitter/PartyMemberIcon.js
+++ components/TreasureSplitter/PartyMemberIcon.js
@@ -1,7 +1,14 @@
 export default ({ icon, size="38" }) => (
-  <img className="icon"
-    src={icon ? icon : '/img/default-avatar.png'}
-    width={size}
-    height={size}
-  />
+  <>
+    <img className="icon"
+      src={icon ? icon : '/img/default-avatar.png'}
+      width={size}
+      height={size}
+    />
+    <style jsx>{`
+    .icon {
+      background-color: #c9c9c9;
+    }
+    `}</style>
+  </>
 )

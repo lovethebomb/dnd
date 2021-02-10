814247c372c097e9b795cdd247a782eb27a10541
Lucas Heymès <lucas@lovethebomb.eu>
2021-02-10 13:41:41 +0100

    Force fathom-client to 2.0.3

--- pages/_app.js
+++ pages/_app.js
@@ -13,21 +13,22 @@ export default function Application({ Component, pageProps }) {
   const router = useRouter()
 
   useEffect(() => {
-    // Initialize Fathom when the app loads
-    Fathom.load('AWEBH', {
-      url: 'https://fathom.ilotreseau.net/tracker.js',
-      includedDomains: ['dnd.lucas.computer'],
-    })
-
-    function onRouteChangeComplete() {
+    if (process.env.NODE_ENV === 'production') {
+      // Initialize Fathom when the app loads
+      Fathom.load('//fathom.ilotreseau.net/tracker.js')
+      Fathom.setSiteId('AWEBH')
       Fathom.trackPageview()
-    }
-    // Record a pageview when route changes
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
 
-    // Unassign event listener
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
+      function onRouteChangeComplete() {
+        Fathom.trackPageview()
+      }
+      // Record a pageview when route changes
+      router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+      // Unassign event listener
+      return () => {
+        router.events.off('routeChangeComplete', onRouteChangeComplete)
+      }
     }
   }, [])
 

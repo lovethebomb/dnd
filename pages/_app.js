d2dd9475ffc5ed6f7e0cf446ab60543ac4c50003
Lucas Heymès <lucas@lovethebomb.eu>
2021-02-10 12:19:54 +0100

    Add Fathom client

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,36 @@
-import '../styles.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as Fathom from 'fathom-client'
+import { ModalProvider } from 'react-modal-hook';
+
 import { DarkModeProvider } from '../components/context/DarkModeContext'
-import { ModalProvider } from "react-modal-hook";
+
+import '../styles.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function Application({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('AWEBH', {
+      url: 'https://fathom.ilotreseau.net/tracker.js',
+      includedDomains: ['dnd.lucas.computer'],
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
   return (
     <DarkModeProvider>
       <ModalProvider>
